ae3934f1789585617618b7815f815c2e915bebd9
unknown <GBEGUIN@BTW70984.bt0d0000.w2k.bouyguestelecom.fr>
2015-09-03 17:06:39 +0200

    [FIX] Fix issue on root directory

--- app/src/main/java/com/freak/asteroid/randomiser/MainActivity.java
+++ app/src/main/java/com/freak/asteroid/randomiser/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        File root = new File(Environment.getExternalStorageDirectory() + "/Music");
+        File root = new File(Environment.getExternalStorageDirectory() + "/Musique");
         RandomiseThread randomiser = new RandomiseThread(root);
         randomiser.start();
     }
